6f1e36967336a27dd5c803be9900f7e0a66c8a5f
JonathanLuna95 <jjlunap95@gmail.com>
2026-05-12 16:27:42 -0500

    Agrega pruebas de cuadrados en el tablero

--- serpiente.js
+++ serpiente.js
@@ -22,6 +22,12 @@
     function dibujarTodo() {
       limpiarCanvas();
       dibujarTablero();
+      pintarParte(5,5);
+      pintarParte(10,2);
+      pintarParte(5,canvas.height/TAMANIO_CELDA-1);
+      pintarParte(canvas.width/TAMANIO_CELDA-1,(canvas.height/TAMANIO_CELDA)/2);
+      pintarParte(0, canvas.height/TAMANIO_CELDA-8);
+      pintarParte(canvas.width/TAMANIO_CELDA-1,canvas.height/TAMANIO_CELDA-1);
     }
 
     function dibujarTablero(){
@@ -41,4 +47,17 @@
       ctx.lineTo(canvas.width,y);
       ctx.stroke();
       }
+    }
+
+    function pintarParte(lineaX,lineaY){
+
+      let valorX = lineaX * TAMANIO_CELDA;
+      let valorY = lineaY * TAMANIO_CELDA;
+
+      ctx.fillStyle = "#ff0000";
+      ctx.fillRect(valorX, valorY, TAMANIO_CELDA, TAMANIO_CELDA);
+
+      ctx.strokeStyle = "#000000";
+      ctx.strokeRect(valorX, valorY, TAMANIO_CELDA, TAMANIO_CELDA);
+
     }
